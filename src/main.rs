6d8150edc1908391b6eb564456ccfc676be41744
Scott MacDonald <scott@smacdo.com>
2024-11-30 22:40:15 -0800

    moving output code into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod y2024;
 
 use advent_of_code_data::{
-    client::WebClient, data::CheckResult, registry::{Solver, SolverError, SolverRegistry}, runner::{RunnerEventHandler, SolverRunner}, Answer, Day, Part, Year
+    client::WebClient,
+    data::CheckResult,
+    registry::{Solver, SolverError, SolverRegistry},
+    runner::{RunnerEventHandler, SolverRunner},
+    Answer, Day, Part, Year,
 };
+use advent_of_code_rust::terminal_output::ConsoleRunnerEventHandler;
 use clap::{Parser, Subcommand};
 use linkme::distributed_slice;
 
@@ -84,9 +89,8 @@ fn main() {
             // TODO: All the days if not specified.
             let requested_days = days.as_ref().expect("TODO: implement default fallback");
             let available_days = solver_registry.days(year);
-            let mut runner = SolverRunner::new(
-                Box::new(client),
-                Box::new(ConsoleRunnerEventHandler{}));
+            let mut runner =
+                SolverRunner::new(Box::new(client), Box::new(ConsoleRunnerEventHandler {}));
 
             for requested_day in requested_days {
                 let requested_day = Day(*requested_day);
@@ -101,88 +105,5 @@ fn main() {
         _ => {
             panic!("command not implemented yet")
         }
-    }
-}
-
-// TODO: measure elapsed times.
-pub struct ConsoleRunnerEventHandler {}
-
-impl RunnerEventHandler for ConsoleRunnerEventHandler {
-    fn on_start_solver(&mut self, _solver: &Solver) {
-        // TODO: start measuring execution time for solver.
-    }
-
-    fn on_part_examples_pass(&mut self, solver: &Solver, part: Part, count: usize) {
-        if count > 0 {
-            println!(
-                "👍 Tested the examples for part {} day {} year {}",
-                part, solver.day, solver.year
-            )
-        }
-
-        // Running the solver with real input happens immediately after this
-        // event, so start the solver timer now.
-        // TODO: implement time recording.
-    }
-
-    fn on_start_part(&mut self, _solver: &Solver, _part: Part) {}
-
-    fn on_finish_part(
-        &mut self,
-        _solver: &Solver,
-        part: Part,
-        result: &Result<(Answer, CheckResult), SolverError>,
-    ) {
-        // Calculate the time elapsed since the examples completed and this event
-        // indicating it finished.
-        // elapsed_seconds = time.time() - self.solver_start_times[
-        //    solver_metadata
-        // ].get_part_start_time(part)
-
-
-        // Catch the examples failed condition early, and print it before trying
-        // to calculate runtime of the solution which isn't possible because the
-        // solution never ran.
-        match result {
-            Ok((answer, CheckResult::Correct)) => {
-                println!("✅ part {part}: {answer} [0.0s]");
-            },
-            Ok((answer, CheckResult::Wrong)) => {
-                println!("❌ Wrong answer for part {part}: {answer} [0.0s]")
-            },
-            Ok((answer, CheckResult::TooLow)) => {
-                println!("❌ Wrong answer for part {part}: {answer} is too low [0.0s]")
-            },
-            Ok((answer, CheckResult::TooHigh)) => {
-                println!("❌ Wrong answer for part {part}: {answer} is too high [0.0s]")
-            },
-            Err(SolverError::NotFinished) => {
-                println!("👻 Solver for part {} is not finished", part);
-            },
-            Err(SolverError::ExampleFailed { input, expected, actual,..})  => {
-                println!(
-                    "👎 The example output for part {} is `{}` but the solver returned `{}` using input:\n```\n{}\n```",
-                    part, 
-                    expected,
-                    actual,
-                    input
-                );
-            }
-            Err(SolverError::TooSoon) => {
-                println!("⏱️ Solution for part {part} submitted too soon, please wait a bit before trying again");
-            }
-            Err(error) => {
-                // TODO: Better error reporting.
-                // TODO: Better icon
-                println!("👎 The solver for part {part} returned an unhandled error: {error:?}");
-            }
-        }
-    }
-
-    fn on_finish_solver(
-        &mut self,
-        _solver: &Solver,
-    ) {
-        // TODO: report total execution time for solver.
     }
 }
---
+++ src/terminal_output.rs
@@ -0,0 +1,85 @@
+use advent_of_code_data::{data::CheckResult, registry::{Solver, SolverError}, runner::RunnerEventHandler, Answer, Part};
+
+
+// TODO: measure elapsed times.
+pub struct ConsoleRunnerEventHandler {}
+
+impl RunnerEventHandler for ConsoleRunnerEventHandler {
+    fn on_start_solver(&mut self, _solver: &Solver) {
+        // TODO: start measuring execution time for solver.
+    }
+
+    fn on_part_examples_pass(&mut self, solver: &Solver, part: Part, count: usize) {
+        if count > 0 {
+            println!(
+                "👍 Tested the examples for part {} day {} year {}",
+                part, solver.day, solver.year
+            )
+        }
+
+        // Running the solver with real input happens immediately after this
+        // event, so start the solver timer now.
+        // TODO: implement time recording.
+    }
+
+    fn on_start_part(&mut self, _solver: &Solver, _part: Part) {}
+
+    fn on_finish_part(
+        &mut self,
+        _solver: &Solver,
+        part: Part,
+        result: &Result<(Answer, CheckResult), SolverError>,
+    ) {
+        // Calculate the time elapsed since the examples completed and this event
+        // indicating it finished.
+        // elapsed_seconds = time.time() - self.solver_start_times[
+        //    solver_metadata
+        // ].get_part_start_time(part)
+
+
+        // Catch the examples failed condition early, and print it before trying
+        // to calculate runtime of the solution which isn't possible because the
+        // solution never ran.
+        match result {
+            Ok((answer, CheckResult::Correct)) => {
+                println!("✅ part {part}: {answer} [0.0s]");
+            },
+            Ok((answer, CheckResult::Wrong)) => {
+                println!("❌ Wrong answer for part {part}: {answer} [0.0s]")
+            },
+            Ok((answer, CheckResult::TooLow)) => {
+                println!("❌ Wrong answer for part {part}: {answer} is too low [0.0s]")
+            },
+            Ok((answer, CheckResult::TooHigh)) => {
+                println!("❌ Wrong answer for part {part}: {answer} is too high [0.0s]")
+            },
+            Err(SolverError::NotFinished) => {
+                println!("👻 Solver for part {} is not finished", part);
+            },
+            Err(SolverError::ExampleFailed { input, expected, actual,..})  => {
+                println!(
+                    "👎 The example output for part {} is `{}` but the solver returned `{}` using input:\n```\n{}\n```",
+                    part, 
+                    expected,
+                    actual,
+                    input
+                );
+            }
+            Err(SolverError::TooSoon) => {
+                println!("⏱️ Solution for part {part} submitted too soon, please wait a bit before trying again");
+            }
+            Err(error) => {
+                // TODO: Better error reporting.
+                // TODO: Better icon
+                println!("👎 The solver for part {part} returned an unhandled error: {error:?}");
+            }
+        }
+    }
+
+    fn on_finish_solver(
+        &mut self,
+        _solver: &Solver,
+    ) {
+        // TODO: report total execution time for solver.
+    }
+}
