083d7fa260e78ca8ecc1f6ff68fc9e0f082b6c09
Scott MacDonald <scott@smacdo.com>
2024-12-02 21:53:10 -0800

    refactor and simplify day 3 year 2024 solution

--- src/y2024/day3.rs
+++ src/y2024/day3.rs
@@ -1,6 +1,5 @@
-use advent_of_code_data::registry::{Result, Solver, SolverError, SolverPart};
+use advent_of_code_data::registry::{Result, Solver, SolverPart};
 use advent_of_code_data::{Answer, Day, Year};
-use advent_of_code_rust::utils;
 use linkme::distributed_slice;
 use regex::Regex;
 
@@ -26,61 +25,38 @@ static SOLVER: Solver = Solver {
     },
 };
 
-// TODO: no need to iterate on lines
-
 pub fn day_3_1(input: &str) -> Result<Answer> {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     let mut mul_sum: i64 = 0;
 
-    for line in input.lines() {
-        for (_, [left, right]) in re.captures_iter(line).map(|c| c.extract()) {
-            mul_sum += left.parse::<i64>().unwrap() * right.parse::<i64>().unwrap();
-        }
+    for (_, [left, right]) in re.captures_iter(input).map(|c| c.extract()) {
+        mul_sum += left.parse::<i64>().unwrap() * right.parse::<i64>().unwrap();
     }
 
     Ok(mul_sum.into())
 }
 
 pub fn day_3_2(input: &str) -> Result<Answer> {
-    let re =
-        Regex::new(r"(?<cmd>(mul)|(do)|(don't))\(((?<left>\d{1,3}),(?<right>\d{1,3}))?\)").unwrap();
+    let re = Regex::new(
+        r"(?<mul>mul\((?<left>\d{1,3}),(?<right>\d{1,3})\))|(?<do>do\(\))|(?<dont>don't\(\))",
+    )
+    .unwrap();
+
     let mut mul_sum: i64 = 0;
     let mut mul_enabled = true;
 
-    for line in input.lines() {
-        for c in re.captures_iter(line) {
-            match c.name("cmd").unwrap().as_str() {
-                "mul" => {
-                    match (c.name("left"), c.name("right")) {
-                        (Some(left), Some(right)) => {
-                            if mul_enabled {
-                                mul_sum += left.as_str().parse::<i64>().unwrap()
-                                    * right.as_str().parse::<i64>().unwrap();
-                            }
-                        }
-                        _ => {
-                            // invalid - mul is missing required left/right args. Ignore!
-                        }
-                    }
-                }
-                "do" => {
-                    if c.name("left").is_none() && c.name("right").is_none() {
-                        mul_enabled = true;
-                    } else {
-                        // invalid - do cannot have args. Ignore!
-                    }
-                }
-                "don't" => {
-                    if c.name("left").is_none() && c.name("right").is_none() {
-                        mul_enabled = false;
-                    } else {
-                        // invalid - do cannot have args. Ignore!
-                    }
-                }
-                _ => {
-                    // unknown command, ignore!
-                }
+    for c in re.captures_iter(input) {
+        if c.name("mul").is_some() {
+            if mul_enabled {
+                let left = c.name("left").unwrap().as_str();
+                let right = c.name("right").unwrap().as_str();
+
+                mul_sum += left.parse::<i64>().unwrap() * right.parse::<i64>().unwrap();
             }
+        } else if c.name("do").is_some() {
+            mul_enabled = true;
+        } else if c.name("dont").is_some() {
+            mul_enabled = false;
         }
     }
 
