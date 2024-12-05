b88b498e4716416ae470872568a6b701ceb59869
Scott MacDonald <scott@smacdo.com>
2024-12-04 20:48:33 -0800

    clean up day 4 year 2024 solution with new grid and point

--- src/y2024/day4.rs
+++ src/y2024/day4.rs
@@ -1,5 +1,8 @@
+use std::str::FromStr;
+
 use advent_of_code_data::registry::{Result, Solver, SolverPart};
 use advent_of_code_data::{Answer, Day, Year};
+use advent_of_code_rust::spatial::{Grid, Point2};
 use linkme::distributed_slice;
 
 use crate::SOLVERS;
@@ -42,41 +45,31 @@ M.M.M.M.M.
     },
 };
 
-pub fn is_word(grid: &[Vec<char>], word: &str, x: usize, y: usize, dir: &(i32, i32)) -> bool {
-    let mut x = x as i32;
-    let mut y = y as i32;
+// TODO: use directions.
+pub fn is_word(grid: &Grid<char>, word: &str, x: isize, y: isize, dir: &(isize, isize)) -> bool {
+    let mut pos = Point2::new(x, y);
 
     for i in 0..word.len() {
-        if x < 0 || y < 0 || y as usize >= grid.len() || x as usize >= grid[y as usize].len() {
+        if !grid.is_pos_in_bounds(pos) {
             return false;
         }
 
-        if grid[y as usize][x as usize] != word.chars().nth(i).unwrap() {
+        if grid[pos] != word.chars().nth(i).unwrap() {
             return false;
         }
 
-        x += dir.0;
-        y += dir.1;
+        pos += Point2::new(dir.0, dir.1);
     }
 
     true
 }
 
 pub fn day_4_1(input: &str) -> Result<Answer> {
-    // Build row major grid.
-    let mut grid: Vec<Vec<char>> = Vec::new();
-    let mut visited: Vec<Vec<bool>> = Vec::new();
-
-    for line in input.lines() {
-        grid.push(line.chars().collect());
-        visited.push((0..line.len()).map(|_| false).collect());
-    }
-
-    // Search each tile to see if it is a valid XMAS.
+    let grid = Grid::from_str(input).unwrap();
     let mut xmas_count = 0;
 
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
+    for y in 0..(grid.x_count() as isize) {
+        for x in 0..(grid.y_count() as isize) {
             let dirs = &[
                 (1, 0),
                 (-1, 0),
@@ -99,47 +92,21 @@ pub fn day_4_1(input: &str) -> Result<Answer> {
     Ok(xmas_count.into())
 }
 
-fn is_mas(x1: i32, y1: i32, x2: i32, y2: i32, grid: &[Vec<char>]) -> bool {
-    if x1 < 0
-        || x2 < 0
-        || y1 < 0
-        || y2 < 0
-        || y1 as usize >= grid.len()
-        || y2 as usize >= grid.len()
-        || x1 as usize >= grid[y1 as usize].len()
-        || x2 as usize >= grid[y2 as usize].len()
-    {
-        false
-    } else {
-        let y1 = y1 as usize;
-        let y2 = y2 as usize;
-        let x1 = x1 as usize;
-        let x2 = x2 as usize;
-
-        (grid[y1][x1] == 'M' && grid[y2][x2] == 'S') || (grid[y2][x2] == 'M' && grid[y1][x1] == 'S')
-    }
+fn is_mas(grid: &Grid<char>, a: Point2, b: Point2) -> bool {
+    grid.is_pos_in_bounds(a)
+        && grid.is_pos_in_bounds(b)
+        && ((grid[a] == 'M' && grid[b] == 'S') || (grid[b] == 'M' && grid[a] == 'S'))
 }
 
 pub fn day_4_2(input: &str) -> Result<Answer> {
-    // Build row major grid.
-    let mut grid: Vec<Vec<char>> = Vec::new();
-    let mut visited: Vec<Vec<bool>> = Vec::new();
-
-    for line in input.lines() {
-        grid.push(line.chars().collect());
-        visited.push((0..line.len()).map(|_| false).collect());
-    }
-
+    let grid = Grid::from_str(input).unwrap();
     let mut xmas_count = 0;
 
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
-            let dx = x as i32;
-            let dy = y as i32;
-
-            if grid[y][x] == 'A'
-                && is_mas(dx - 1, dy - 1, dx + 1, dy + 1, &grid)
-                && is_mas(dx - 1, dy + 1, dx + 1, dy - 1, &grid)
+    for y in 0..(grid.y_count() as isize) {
+        for x in 0..(grid.x_count() as isize) {
+            if grid[Point2::new(x, y)] == 'A'
+                && is_mas(&grid, Point2::new(x - 1, y - 1), Point2::new(x + 1, y + 1))
+                && is_mas(&grid, Point2::new(x - 1, y + 1), Point2::new(x + 1, y - 1))
             {
                 xmas_count += 1
             }
