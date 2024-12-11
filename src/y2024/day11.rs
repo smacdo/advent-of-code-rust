d4b7d36c32a2b6c1d67573763f283b63a6e0c14b
Scott MacDonald <scott@smacdo.com>
2024-12-10 22:15:55 -0800

    solve day 11 year 2024 part 2

--- src/y2024/day11.rs
+++ src/y2024/day11.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
+
 use advent_of_code_data::registry::{Result, Solver, SolverPart};
 use advent_of_code_data::{Answer, Day, Year};
 use advent_of_code_rust::utils;
-use either::{Left, Right};
 use linkme::distributed_slice;
 
 use crate::SOLVERS;
@@ -16,64 +17,76 @@ static SOLVER: Solver = Solver {
     },
     part_two: SolverPart {
         func: day_11_2,
-        examples: &[
-            //(Answer::Int(0), "Example input",)
-        ],
+        examples: &[(Answer::Int(65601038650482), "125 17")],
     },
 };
 
-fn blink(stones: &[i64]) -> Vec<i64> {
-    stones
-        .iter()
-        .flat_map(|stone| {
-            // NOTE: ilog10 counts the number of digits
-            // https://stackoverflow.com/a/24177168
-            let digit_count = stone.checked_ilog10().map(|x| x + 1);
-            // println!("{stone} digit count is {digit_count:?}");
+fn blink(stones: &HashMap<i64, usize>) -> HashMap<i64, usize> {
+    let mut new_stones: HashMap<i64, usize> = HashMap::new();
 
-            match digit_count {
-                None => Left(std::iter::once(1)),
-                Some(digit_count) if digit_count % 2 == 0 => {
-                    // Extract left and right sides.
-                    let half_digit_count = digit_count / 2;
-                    let mut left = *stone;
-                    let mut right = 0;
+    for (stone, count) in stones {
+        // NOTE: ilog10 counts the number of digits
+        // https://stackoverflow.com/a/24177168
+        let digit_count = stone.checked_ilog10().map(|x| x + 1);
 
-                    for i in 0..half_digit_count {
-                        let digit = left % 10;
-                        left /= 10;
-                        right += digit * 10_i64.pow(i);
-                    }
-
-                    // println!("{stone} splits into {left} and {right}");
-
-                    Right(std::iter::once(left).chain(std::iter::once(right)))
-                }
-                Some(_) => Left(std::iter::once(*stone * 2024)),
+        match digit_count {
+            None => {
+                *new_stones.entry(1).or_default() += count;
             }
-        })
-        .collect()
+            Some(digit_count) if digit_count % 2 == 0 => {
+                // Extract left and right sides.
+                let half_digit_count = digit_count / 2;
+                let mut left = *stone;
+                let mut right = 0;
+
+                for i in 0..half_digit_count {
+                    let digit = left % 10;
+                    left /= 10;
+                    right += digit * 10_i64.pow(i);
+                }
+
+                // println!("{stone} splits into {left} and {right}");
+                *new_stones.entry(left).or_default() += count;
+                *new_stones.entry(right).or_default() += count;
+            }
+            Some(_) => {
+                *new_stones.entry(*stone * 2024).or_default() += count;
+            }
+        }
+    }
+
+    new_stones
+}
+
+fn parse_input(input: &str) -> HashMap<i64, usize> {
+    let mut new_stones: HashMap<i64, usize> = HashMap::new();
+
+    for stone in utils::find_ints(input) {
+        *new_stones.entry(stone).or_default() += 1;
+    }
+
+    new_stones
 }
 
 pub fn day_11_1(input: &str) -> Result<Answer> {
-    let mut stones = utils::find_ints(input);
+    let mut stones = parse_input(input);
 
     for _ in 0..25 {
         stones = blink(&stones);
     }
 
-    Ok(stones.len().into())
+    Ok(stones.values().sum::<usize>().into())
 }
 
 pub fn day_11_2(input: &str) -> Result<Answer> {
-    let mut stones = utils::find_ints(input);
+    let mut stones = parse_input(input);
 
-    for i in 0..75 {
+    for _ in 0..75 {
         stones = blink(&stones);
-        println!("{i} has {} stones", stones.len());
+        //println!("{i} has {} stones", stones.values().sum::<usize>());
     }
 
-    Ok(stones.len().into())
+    Ok(stones.values().sum::<usize>().into())
 }
 
 #[cfg(test)]
@@ -82,8 +95,8 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        let stones = utils::find_ints("0 1 10 99 999");
-        assert_eq!(blink(&stones), vec![1, 2024, 1, 0, 9, 9, 2021976]);
+        let stones = parse_input("0 1 10 99 999");
+        assert_eq!(blink(&stones), parse_input("1 2024 1 0 9 9 2021976"));
 
         //assert_eq!(blink(&stones), vec![]);
     }
