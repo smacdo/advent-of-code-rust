49e37ffee3bb28f6e91ba66c0bfff89fb21f63b2
Scott MacDonald <scott@smacdo.com>
2025-01-02 18:26:06 -0600

    move timing from application to runner

--- crates/advent-of-code-data/src/runner.rs
+++ crates/advent-of-code-data/src/runner.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use thiserror::Error;
 
 use crate::{
@@ -7,25 +9,36 @@ use crate::{
     Answer, Part,
 };
 
-#[derive(Error, Debug)]
-pub enum RunnerError {
-    #[error(transparent)]
-    Client(#[from] ClientError),
-    #[error(transparent)]
-    Solver(#[from] SolverError),
-}
+// TODO:
+//  - on_start_solver(solver)
+//    - on_start_part(solver, part)
+//      - on_start_examples(solver, part)
+//        - on_start_example(solver, part, example_index)
+//        _ on_finish_example(solver, part, example_index, duration, result)
+//      - on_finish_examples(solver, part, did_all_pass)
+//      - on_start_solver_part(solver, part)
+//      - on_finish_solver_part(solver, part, duration, result)
+//   - on_finish_part(solver, part, duration, result)
+// - on_finish_solver(solver, duration, result)
 
 pub trait RunnerEventHandler {
     fn on_start_solver(&mut self, solver: &Solver);
-    fn on_part_examples_pass(&mut self, solver: &Solver, part: Part, count: usize);
+    fn on_part_examples_pass(
+        &mut self,
+        solver: &Solver,
+        part: Part,
+        duration: Duration,
+        count: usize,
+    );
     fn on_start_part(&mut self, solver: &Solver, part: Part);
     fn on_finish_part(
         &mut self,
         solver: &Solver,
         part: Part,
-        result: &Result<(Answer, CheckResult), RunnerError>,
+        duration: Duration,
+        result: &Result<RunPartDetails, RunnerError>,
     );
-    fn on_finish_solver(&mut self, solver: &Solver);
+    fn on_finish_solver(&mut self, solver: &Solver, duration: Duration, details: RunDetails);
 }
 
 pub struct SolverRunner {
@@ -68,12 +81,11 @@ impl SolverRunner {
             solver.year
         );
 
-        let parts: [Part; 2] = [Part::One, Part::Two];
-        let input = client.get_input(solver.day, solver.year).unwrap();
-
         events.on_start_solver(solver);
 
-        for part in parts {
+        let mut event_details = SolverEventDetails::new();
+
+        for part in [Part::One, Part::Two] {
             let solver_part = solver.part(part);
 
             events.on_start_part(solver, part);
@@ -81,11 +93,15 @@ impl SolverRunner {
             // Validate examples listed for the current part prior to running the
             // part on real input. Use all of the examples associated with the
             // solver unless the caller has requested a specific example be run.
+            let all_examples_start_time = Instant::now();
+
             let mut examples_pass = true;
             let examples = solver_part.examples;
 
             for example in examples {
+                let example_start_time = Instant::now();
                 let result = (solver_part.func)(example.1);
+                let example_duration = Instant::now() - example_start_time;
 
                 match result {
                     Ok(answer) => {
@@ -95,6 +111,7 @@ impl SolverRunner {
                             events.on_finish_part(
                                 solver,
                                 part,
+                                example_duration,
                                 &Err(SolverError::ExampleFailed {
                                     part,
                                     input: example.1.to_string(),
@@ -117,29 +134,105 @@ impl SolverRunner {
             // Notify the event manager that examples have passed, otherwise if
             // any have failed then skip running the part with real input.
             if examples_pass {
-                events.on_part_examples_pass(solver, part, examples.len())
+                let all_examples_duration = Instant::now() - all_examples_start_time;
+                events.on_part_examples_pass(solver, part, all_examples_duration, examples.len())
             } else {
                 continue;
             }
 
-            // Run the solver against real puzzle input.
-            // TODO: measure execution time without including anwers io cache time.
-            //       call on_finish_part immediately after solver result
-            let solver_result = (solver_part.func)(&input);
+            // Fetch input only after examples have passed, but before we start
+            // timing the execution of the solver.
+            let input = client.get_input(solver.day, solver.year).unwrap();
 
-            let final_result: Result<(Answer, CheckResult), RunnerError> = match solver_result {
+            // Run the solver against real puzzle input.
+            let solve_start_time = Instant::now();
+            let solver_result = (solver_part.func)(&input);
+            let solve_duration = Instant::now() - solve_start_time;
+
+            let final_result: Result<RunPartDetails, RunnerError> = match solver_result {
                 Ok(answer) => {
                     match client.submit_answer(answer.clone(), part, solver.day, solver.year) {
-                        Ok(check_result) => Ok((answer, check_result)),
+                        Ok(check_result) => Ok(RunPartDetails {
+                            answer,
+                            check_result,
+                            duration: solve_duration,
+                        }),
                         Err(err) => Err(err.into()),
                     }
                 }
                 Err(err) => Err(err.into()),
             };
 
-            events.on_finish_part(solver, part, &final_result);
+            events.on_finish_part(solver, part, solve_duration, &final_result);
+            event_details.set_part_result(part, final_result);
         }
 
-        events.on_finish_solver(solver);
+        let run_details: RunDetails = event_details.into();
+        events.on_finish_solver(solver, run_details.duration, run_details);
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum RunnerError {
+    #[error(transparent)]
+    Client(#[from] ClientError),
+    #[error(transparent)]
+    Solver(#[from] SolverError),
+}
+
+#[derive(Debug)]
+pub struct RunPartDetails {
+    pub answer: Answer,
+    pub check_result: CheckResult,
+    pub duration: Duration,
+}
+
+#[derive(Debug)]
+pub struct RunDetails {
+    pub part_one_result: Result<RunPartDetails, RunnerError>,
+    pub part_two_result: Result<RunPartDetails, RunnerError>,
+    pub duration: Duration,
+}
+
+// TODO: measure example time, solver time and account for input time.
+#[derive(Debug)]
+struct SolverEventDetails {
+    pub start_time: Instant,
+    pub finish_time: Option<Instant>,
+    pub part_one_result: Option<Result<RunPartDetails, RunnerError>>,
+    pub part_two_result: Option<Result<RunPartDetails, RunnerError>>,
+}
+
+impl SolverEventDetails {
+    fn new() -> Self {
+        Self {
+            start_time: Instant::now(),
+            finish_time: None,
+            part_one_result: None,
+            part_two_result: None,
+        }
+    }
+
+    fn set_part_result(&mut self, part: Part, result: Result<RunPartDetails, RunnerError>) {
+        match part {
+            Part::One => self.part_one_result = Some(result),
+            Part::Two => self.part_two_result = Some(result),
+        }
+    }
+}
+
+impl Default for SolverEventDetails {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl From<SolverEventDetails> for RunDetails {
+    fn from(value: SolverEventDetails) -> Self {
+        RunDetails {
+            part_one_result: value.part_one_result.unwrap(),
+            part_two_result: value.part_two_result.unwrap(),
+            duration: value.finish_time.unwrap_or(Instant::now()) - value.start_time,
+        }
     }
 }
--- src/terminal_output.rs
+++ src/terminal_output.rs
@@ -1,36 +1,14 @@
-use std::{collections::HashMap, time::Instant};
+use std::time::Duration;
 
-use advent_of_code_data::{data::CheckResult, registry::{Solver, SolverError}, runner::{RunnerError, RunnerEventHandler}, Answer, Day, Part, Year};
-
-struct SolverTimes {
-    pub part_one_start: Option<Instant>,
-    pub part_two_start: Option<Instant>,
-}
-
-impl SolverTimes {
-    pub fn new() -> Self {
-        Self {
-            part_one_start: None,
-            part_two_start: None,
-        }
-    }
-}
-
-impl Default for SolverTimes {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+use advent_of_code_data::{data::CheckResult, registry::{Solver, SolverError}, runner::{RunDetails, RunPartDetails, RunnerError, RunnerEventHandler}, Part};
 
 // TODO: measure elapsed times.
 pub struct ConsoleRunnerEventHandler {
-    times: HashMap<(Day, Year), SolverTimes>,
 }
 
 impl ConsoleRunnerEventHandler {
     pub fn new() -> Self {
         Self {
-            times: HashMap::new()
         }
     }
 }
@@ -42,98 +20,69 @@ impl Default for ConsoleRunnerEventHandler {
 }
 
 impl RunnerEventHandler for ConsoleRunnerEventHandler {
-    fn on_start_solver(&mut self, solver: &Solver) {
-        self.times.insert((solver.day, solver.year), SolverTimes::new());
+    fn on_start_solver(&mut self, _solver: &Solver) {
     }
 
-    fn on_part_examples_pass(&mut self, solver: &Solver, part: Part, count: usize) {
+    fn on_part_examples_pass(&mut self, solver: &Solver, part: Part, duration: Duration, count: usize) {
         if count > 0 {
             println!(
-                "👍 Tested the examples for part {} day {} year {}",
-                part, solver.day, solver.year
+                "👍 Tested the examples for part {} day {} year {} [{:.3}s]",
+                part, solver.day, solver.year, duration.as_secs_f32()
             )
-        }
-
-        // Running the solver with real input happens immediately after this
-        // event, so start the solver timer now.
-        match part {
-            Part::One => {
-                self.times.get_mut(&(solver.day, solver.year)).unwrap().part_one_start = Some(Instant::now());
-            } ,
-            Part::Two => {
-                self.times.get_mut(&(solver.day, solver.year)).unwrap().part_two_start = Some(Instant::now());
-            },
         }
     }
 
-    fn on_start_part(&mut self, solver: &Solver, part: Part) {
-        match part {
-            Part::One => {
-                self.times.get_mut(&(solver.day, solver.year)).unwrap().part_one_start = Some(Instant::now());
-            } ,
-            Part::Two => {
-                self.times.get_mut(&(solver.day, solver.year)).unwrap().part_two_start = Some(Instant::now());
-            },
-        }
+    fn on_start_part(&mut self, _solver: &Solver, _part: Part) {
     }
 
     fn on_finish_part(
         &mut self,
-        solver: &Solver,
+        _solver: &Solver,
         part: Part,
-        result: &Result<(Answer, CheckResult), RunnerError>,
+        duration: Duration,
+        result: &Result<RunPartDetails, RunnerError>,
     ) {
-        // Calculate the time elapsed since the examples completed and this event
-        // indicating it finished.
-        // elapsed_seconds = time.time() - self.solver_start_times[
-        //    solver_metadata
-        // ].get_part_start_time(part)
-        let start_time = match part {
-            Part::One => {
-                self.times.get(&(solver.day, solver.year)).unwrap().part_one_start.unwrap()
-            } ,
-            Part::Two => {
-                self.times.get(&(solver.day, solver.year)).unwrap().part_two_start.unwrap()
-            },
-        };
-        let elapsed_time = Instant::now() - start_time;
-
         // Catch the examples failed condition early, and print it before trying
         // to calculate runtime of the solution which isn't possible because the
         // solution never ran.
         match result {
-            Ok((answer, CheckResult::Correct)) => {
-                println!("✅ part {part}: {answer} [{:.3}s]", elapsed_time.as_secs_f32());
+            Ok(RunPartDetails{answer, check_result: CheckResult::Correct, ..}) => {
+                println!("✅ part {part}: {answer} [{:.3}s]", duration.as_secs_f32());
             },
-            Ok((answer, CheckResult::Wrong)) => {
-                println!("❌ Wrong answer for part {part}: {answer} [{:.3}s]", elapsed_time.as_secs_f32())
+            Ok(RunPartDetails{answer, check_result: CheckResult::Wrong, ..}) => {
+                println!("❌ Wrong answer for part {part}: {answer} [{:.3}s]", duration.as_secs_f32())
             },
-            Ok((answer, CheckResult::TooLow)) => {
-                println!("❌ Wrong answer for part {part}: {answer} is too low [{:.3}s]", elapsed_time.as_secs_f32())
+            Ok(RunPartDetails{answer, check_result: CheckResult::TooLow, ..}) => {
+                println!("❌ Wrong answer for part {part}: {answer} is too low [{:.3}s]", duration.as_secs_f32())
             },
-            Ok((answer, CheckResult::TooHigh)) => {
-                println!("❌ Wrong answer for part {part}: {answer} is too high [{:.3}s]", elapsed_time.as_secs_f32())
+            Ok(RunPartDetails{answer, check_result: CheckResult::TooHigh, ..}) => {
+                println!("❌ Wrong answer for part {part}: {answer} is too high [{:.3}s]", duration.as_secs_f32())
             },
             Err(RunnerError::Solver(SolverError::NotFinished)) => {
-                println!("👻 Solver for part {} is not finished [{:.3}s]", part, elapsed_time.as_secs_f32());
+                println!("👻 Solver for part {} is not finished  [{:.3}s]", part, duration.as_secs_f32());
             },
             Err(RunnerError::Solver(SolverError::ExampleFailed { input, expected, actual,..}) ) => {
                 println!(
-                    "👎 The example output for part {} is `{}` but the solver returned `{}` using input [{:.3}s]:\n```\n{}\n```",
+                    "Example output for part {} is `{}` but the solver returned `{}` using input:\n```\n{}\n```",
                     part, 
                     expected,
                     actual,
-                    elapsed_time.as_secs_f32(),
                     input
+                );
+                println!(
+                    "👎 The solver for part {} returned `{}` but the example output is `{}`",
+                    part, 
+                    actual,
+                    expected,
                 );
             }
             Err(RunnerError::Solver(SolverError::TooSoon)) => {
-                println!("⏱️ Solution for part {part} submitted too soon, please wait a bit before trying again");
+                println!("⏱️ Solution for part {part} submitted too soon, please wait a bit before trying again  [{:.3}s]", duration.as_secs_f32());
             }
             Err(error) => {
                 // TODO: Better error reporting.
                 // TODO: Better icon
-                println!("👎 The solver for part {part} returned an unhandled error [{:.3}s]: {error:?}", elapsed_time.as_secs_f32());
+                println!("👎 The solver for part {part} returned an unhandled error [{:.3}s]: {error:?}", duration.as_secs_f32());
             }
         }
     }
@@ -141,7 +90,8 @@ impl RunnerEventHandler for ConsoleRunnerEventHandler {
     fn on_finish_solver(
         &mut self,
         _solver: &Solver,
+        _duration: Duration,
+        _details: RunDetails,
     ) {
-        // TODO: report total execution time for solver.
     }
 }
