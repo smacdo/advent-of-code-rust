17d12c1d46b7101e5c66c273bc14a4ee24d8ec19
Scott MacDonald <scott@smacdo.com>
2025-12-05 19:41:41 -0800

    solved day 5 part 2
    (was actually last night but i was too tired to remember to commit my
    changes!)

--- src/y2025/day5.rs
+++ src/y2025/day5.rs
@@ -19,10 +19,10 @@ static SOLVER: yt::SolverAutoRegister = yt::SolverAutoRegister {
     },
     part_two: yt::SolverPart {
         func: day_5_2,
-        examples: &[/*yt::Example {
-            input: "",
-            expected: aoc::Answer::Int(0),
-        }*/],
+        examples: &[yt::Example {
+            input: "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32",
+            expected: aoc::Answer::Int(14),
+        }],
     },
 };
 
@@ -41,6 +41,38 @@ fn parse_range(input: &str) -> (usize, usize) {
         last.parse()
             .unwrap_or_else(|_| panic!("invalid number of last `{last}`")),
     )
+}
+
+fn merge_ranges(mut ranges: Vec<RangeInclusive<usize>>) -> Vec<RangeInclusive<usize>> {
+    ranges.sort_by_key(|r| *r.start());
+    //eprintln!("RANGES: {ranges:?}");
+
+    let mut merged: Vec<RangeInclusive<usize>> = Vec::new();
+
+    if let Some(first_element) = ranges.first().cloned() {
+        merged.push(first_element);
+
+        for i in 1..ranges.len() {
+            let current = ranges[i].clone();
+            let merged_last = merged.last().unwrap().clone();
+
+            if current.start() <= merged_last.end() {
+                let start = merged_last.start();
+                let last = if current.end() > merged_last.end() {
+                    current.end()
+                } else {
+                    merged_last.end()
+                };
+
+                let last_index = merged.len() - 1;
+                merged[last_index] = *start..=*last;
+            } else {
+                merged.push(current);
+            }
+        }
+    }
+
+    merged
 }
 
 fn parse_database(input: &str) -> Database {
@@ -79,8 +111,17 @@ pub fn day_5_1(args: &yt::SolverArgs) -> yt::Result<aoc::Answer> {
     Ok(sum.into())
 }
 
-pub fn day_5_2(_args: &yt::SolverArgs) -> yt::Result<aoc::Answer> {
-    Err(yt::SolverError::NotFinished)
+pub fn day_5_2(args: &yt::SolverArgs) -> yt::Result<aoc::Answer> {
+    let db = parse_database(args.input);
+    let ranges = merge_ranges(db.fresh_ranges);
+    let mut sum: usize = 0;
+
+    for r in ranges {
+        //eprintln!("{}-{}", r.start(), r.end());
+        sum += r.end() - r.start() + 1;
+    }
+
+    Ok(sum.into())
 }
 
 #[cfg(test)]
@@ -95,6 +136,14 @@ mod tests {
                 fresh_ranges: vec![(3..=5), (10..=14), (16..=20), (12..=18)],
                 ingredients: vec![1, 5, 8, 11, 17, 32]
             }
+        );
+    }
+
+    #[test]
+    fn merge_ranges_test() {
+        assert_eq!(
+            merge_ranges(vec![(2..=6), (8..=10), (1..=3), (15..=18)]),
+            vec![(1..=6), (8..=10), (15..=18)]
         );
     }
 }
