eedb8d10d992b25b0d4139fc996ccf562943eef3
Scott MacDonald <scott@smacdo.com>
2025-12-08 22:06:47 -0800

    solved day 7 part 2

--- src/y2025/day7.rs
+++ src/y2025/day7.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use advent_of_code_data as aoc;
 use ube::spatial::{Grid, Point2};
@@ -24,10 +24,10 @@ static SOLVER: yt::SolverAutoRegister = yt::SolverAutoRegister {
     },
     part_two: yt::SolverPart {
         func: day_7_2,
-        examples: &[/*yt::Example {
-            input: "",
-            expected: aoc::Answer::Int(0),
-        }*/],
+        examples: &[yt::Example {
+            input: ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............",
+            expected: aoc::Answer::Int(40),
+        }],
     },
 };
 
@@ -35,8 +35,8 @@ pub fn find_start(grid: &Grid<char>) -> Option<Point2> {
     grid.points().find(|&pt| grid[pt] == START_CHAR)
 }
 
-pub fn update_beam(grid: &mut Grid<char>, from: Point2) -> usize {
-    let mut pos = from;
+pub fn count_beam_splits(grid: &mut Grid<char>, start: Point2) -> usize {
+    let mut pos = start;
     let mut splitters_count = 0;
 
     while grid.is_pos_in_bounds(pos) {
@@ -51,7 +51,7 @@ pub fn update_beam(grid: &mut Grid<char>, from: Point2) -> usize {
                 for pt in pts {
                     let pos = pt + pos;
                     if grid.is_pos_in_bounds(pos) && grid[pos] == EMPTY_CHAR {
-                        splitters_count += update_beam(grid, pos);
+                        splitters_count += count_beam_splits(grid, pos);
                         did_split = true;
                     }
                 }
@@ -80,15 +80,69 @@ pub fn update_beam(grid: &mut Grid<char>, from: Point2) -> usize {
     splitters_count
 }
 
+pub fn count_timelines(
+    grid: &mut Grid<char>,
+    start: Point2,
+    cache: &mut HashMap<Point2, usize>,
+) -> usize {
+    let mut pos = start;
+    let mut timeline_count = 0;
+
+    while grid.is_pos_in_bounds(pos) {
+        match grid[pos] {
+            EMPTY_CHAR | BEAM_CHAR => {
+                grid[pos] = BEAM_CHAR;
+
+                // Count this as a new timeline each time the beam reaches the bottom of the grid.
+                if pos.y as usize == grid.y_count() - 1 {
+                    timeline_count += 1;
+                }
+            }
+            SPLITTER_CHAR => {
+                let pts = [Point2::WEST, Point2::EAST];
+
+                for pt in pts {
+                    let pos = pt + pos;
+                    if grid.is_pos_in_bounds(pos) && grid[pos] != SPLITTER_CHAR {
+                        if !cache.contains_key(&pos) {
+                            let c = count_timelines(grid, pos, cache);
+                            cache.insert(pos, c);
+                        }
+
+                        timeline_count += cache[&pos];
+                    }
+                }
+
+                break;
+            }
+            START_CHAR => {
+                // ignore!
+            }
+            c => {
+                panic!("unexpected character {} at {}", c, pos);
+            }
+        }
+
+        pos += Point2::SOUTH;
+    }
+
+    timeline_count
+}
+
 pub fn day_7_1(args: &yt::SolverArgs) -> yt::Result<aoc::Answer> {
     let mut grid: Grid<char> = Grid::from_str(args.input).unwrap();
     let start = find_start(&grid).unwrap();
 
-    Ok(update_beam(&mut grid, start).into())
+    Ok(count_beam_splits(&mut grid, start).into())
 }
 
-pub fn day_7_2(_args: &yt::SolverArgs) -> yt::Result<aoc::Answer> {
-    Err(yt::SolverError::NotFinished)
+pub fn day_7_2(args: &yt::SolverArgs) -> yt::Result<aoc::Answer> {
+    let mut grid: Grid<char> = Grid::from_str(args.input).unwrap();
+    let start = find_start(&grid).unwrap();
+
+    let mut cache: HashMap<Point2, usize> = HashMap::new();
+
+    Ok(count_timelines(&mut grid, start, &mut cache).into())
 }
 
 #[cfg(test)]
@@ -103,7 +157,7 @@ mod tests {
         let mut grid: Grid<char> = Grid::from_str(input).unwrap();
         let start = find_start(&grid).unwrap();
 
-        assert_eq!(update_beam(&mut grid, start), 3);
+        assert_eq!(count_beam_splits(&mut grid, start), 3);
 
         let actual = format!("{}", grid);
         assert_eq!(actual.trim(), expected);
